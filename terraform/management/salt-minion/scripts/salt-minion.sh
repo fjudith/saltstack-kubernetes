e4877104bbb2ffa650372df3d4eac4c209f8190a
Florian JUDITH <florian.judith.b@gmail.com>
2018-06-09 00:56:50 +0200

    Fixed salt-minion startup

--- terraform/management/salt-minion/scripts/salt-minion.sh
+++ terraform/management/salt-minion/scripts/salt-minion.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update -yqq
-sudo apt-get install -yqq curl wget
-sudo cat << EOF > /etc/apt/sources.list.d/salt.list
+apt-get update -yqq
+apt-get install -yqq curl wget
+cat << EOF > /etc/apt/sources.list.d/salt.list
 deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/2018.3 xenial main
 EOF
 
-sudo curl -fsSL https://repo.saltstack.com/apt/ubuntu/16.04/amd64/2018.3/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+curl -fsSL https://repo.saltstack.com/apt/ubuntu/16.04/amd64/2018.3/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 
-sudo apt-get clean -yqq
-sudo apt-get update -yqq
-sudo apt-get install -yqq \
+apt-get clean -yqq
+apt-get update -yqq
+apt-get install -yqq \
     salt-minion \
     salt-ssh \
     reclass
 
-
-sudo service salt-minion restart
-sudo systemctl enable salt-minion
+systemctl enable salt-minion
+systemctl daemon-reload
+systemctl restart salt-minion

7164f906451cb0c308a54e41f6e183474613daf2
Florian JUDITH <florian.judith.b@gmail.com>
2018-07-06 20:25:22 +0200

    removed sudo

--- terraform/management/salt-syndic/scripts/salt-syndic.sh
+++ terraform/management/salt-syndic/scripts/salt-syndic.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update -yqq
-sudo apt-get install -yqq curl wget
-sudo cat << EOF > /etc/apt/sources.list.d/salt.list
+apt-get update -yqq
+apt-get install -yqq curl wget
+cat << EOF > /etc/apt/sources.list.d/salt.list
 deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/2018.3 xenial main
 EOF
 
-sudo curl -fsSL https://repo.saltstack.com/apt/ubuntu/16.04/amd64/2018.3/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+curl -fsSL https://repo.saltstack.com/apt/ubuntu/16.04/amd64/2018.3/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 
-sudo apt-get clean -yqq
-sudo apt-get update -yqq
-sudo apt-get install -yqq \
+apt-get clean -yqq
+apt-get update -yqq
+apt-get install -yqq \
     salt-master \
     salt-minion \
     salt-ssh \
@@ -20,10 +20,6 @@ sudo apt-get install -yqq \
     salt-api \
     reclass
 
-sudo service salt-minion restart
-sudo service salt-master restart
-sudo service salt-syndic restart
-
-sudo systemctl enable salt-minion
-sudo systemctl enable salt-master
-sudo systemctl enable salt-syndic
+systemctl enable salt-minion
+systemctl enable salt-master
+systemctl enable salt-syndic

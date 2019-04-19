5b5f0487ac2ba6b8f89cf20bf0c4bda5dc1bcfa0
Florian JUDITH <florian.judith.b@gmail.com>
2019-04-19 21:49:38 +0200

    Setup wildcard domain as A record

--- terraform/dns/cloudflare/main.tf
+++ terraform/dns/cloudflare/main.tf
@@ -26,6 +26,16 @@ resource "cloudflare_record" "wildcard" {
 
   domain  = "${var.domain}"
   name    = "*"
+  value   = "${element(var.public_ips, 0)}"
+  type    = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "kubernetes" {
+  depends_on = ["cloudflare_record.domain"]
+
+  domain  = "${var.domain}"
+  name    = "kubernetes"
   value   = "${var.domain}"
   type    = "CNAME"
   proxied = false

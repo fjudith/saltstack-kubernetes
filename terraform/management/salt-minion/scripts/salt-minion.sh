9c508df25b78d77c8ee5300465419239ed7f8d64
Florian JUDITH <florian.judith.b@gmail.com>
2019-06-24 14:04:32 +0200

    Use Salt-Minion Python3 version

--- terraform/management/salt-minion/scripts/salt-minion.sh
+++ terraform/management/salt-minion/scripts/salt-minion.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 set -e
 
-apt-get update -yqq && apt-get install -yqq curl wget
+SALT_VERSION=2019.2.0
+DEBIAN_FRONTEND=noninteractive
 
-cat << EOF > /etc/apt/sources.list.d/saltstack.list
-deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/2019.2 bionic main
-EOF
-
-curl -fsSL https://repo.saltstack.com/apt/ubuntu/18.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-
-apt-get clean -yqq
-apt-get update -yqq
-apt-get install -yqq \
-    salt-minion \
-    salt-ssh \
-    python-boto \
-    python-boto3 \
-    python-pyinotify \
-    python-psutil \
-    reclass
-
-systemctl enable salt-minion
-
-systemctl daemon-reload
-
+apt-get update -yqq && \
+apt-get install -yqq --no-install-recommends curl net-tools gnupg2  && \
+curl -fsSL http://repo.saltstack.com/py3/ubuntu/18.04/amd64/archive/${SALT_VERSION}/SALTSTACK-GPG-KEY.pub | sudo apt-key add - && \
+echo "deb http://repo.saltstack.com/py3/ubuntu/18.04/amd64/archive/${SALT_VERSION} bionic main" > /etc/apt/sources.list.d/saltstack.list && \
+echo "install salt-master and salt-api, dependencies" && \
+apt-get update -yqq && \
+apt-get install --install-suggests -yqq \
+  python3-pip \
+  python3-setuptools \
+  python3-cherrypy3 \
+  python3-pyinotify \
+  python3-ws4py \
+  salt-minion=${SALT_VERSION}* \
+  salt-ssh=${SALT_VERSION}* \
+  reclass && \
+echo "add a user for the frontend salt:salt" && \
+useradd -m -s/bin/bash -p $(openssl passwd -1 salt) salt && \
+systemctl enable salt-minion && \
+systemctl daemon-reload && \
 systemctl restart salt-minion

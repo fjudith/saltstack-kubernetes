bf640af7b02c13bb762955cd6b49f649e4270614
Florian JUDITH <florian.judith.b@gmail.com>
2019-11-01 00:25:06 +0100

    added hetzner cloud network support

--- terraform/provider/hcloud/main.tf
+++ terraform/provider/hcloud/main.tf
@@ -1,6 +1,21 @@
 ##################################################
 # proxy01
 ##################################################
+resource "hcloud_network" "private" {
+  name = "private"
+  ip_range = "10.0.0.0/8"
+  labels = {
+    app = "kubernetes"
+  }
+}
+
+resource "hcloud_network" "kubernetes" {
+  network_id = "${hcloud_network.mynet.id}"
+  type = "server"
+  network_zone = "eu-central"
+  ip_range = "10.0.1.0/24"
+}
+
 resource "hcloud_server" "proxy01" {
   count       = 1
   name        = "proxy01"
@@ -510,4 +525,35 @@ provisioner "remote-exec" {
     content     = "role: node"
     destination = "/etc/salt/grains"
   }
+}
+
+resource "hcloud_server_network" "proxy01" {
+  count = 1
+  server_id = "${hcloud_server.proxy01.id}"
+  network_id = "${hcloud_network.private.id}"
+  ip = "10.0.1.251"
+}
+
+resource "hcloud_server_network" "proxy02" {
+  count = 1
+  server_id = "${hcloud_server.proxy02.id}"
+  network_id = "${hcloud_network.private.id}"
+}
+
+resource "hcloud_server_network" "etcd" {
+  count = "${var.etcd_count}"
+  server_id = "${element(hcloud_server.etcd.id, count.index)}"
+  network_id = "${hcloud_network.private.id}"
+}
+
+resource "hcloud_server_network" "master" {
+  count = "${var.master_count}"
+  server_id = "${element(hcloud_server.master.id, count.index)}"
+  network_id = "${hcloud_network.private.id}"
+}
+
+resource "hcloud_server_network" "node" {
+  count = "${var.node_count}"
+  server_id = "${element(hcloud_server.node.id, count.index)}"
+  network_id = "${hcloud_network.private.id}"
 }

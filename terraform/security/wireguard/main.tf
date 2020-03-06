81550061b8e55b6732c8c9c3926d2f1fcc66f6fe
Florian JUDITH <florian.judith.b@gmail.com>
2020-03-06 02:35:32 +0100

    Wireguard: Disable wireguard-dkms installation

--- terraform/security/wireguard/main.tf
+++ terraform/security/wireguard/main.tf
@@ -35,15 +35,15 @@ resource "null_resource" "wireguard" {
     ]
   }
 
-  provisioner "remote-exec" {
-    script = "${path.module}/scripts/install-kernel-headers.sh"
-  }
+  # provisioner "remote-exec" {
+  #   script = "${path.module}/scripts/install-kernel-headers.sh"
+  # }
 
-  provisioner "remote-exec" {
-    inline = [
-      "DEBIAN_FRONTEND=noninteractive apt-get install -yqq wireguard-dkms wireguard-tools",
-    ]
-  }
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "DEBIAN_FRONTEND=noninteractive apt-get install -yqq wireguard-dkms wireguard-tools",
+  #   ]
+  # }
 
   provisioner "file" {
     content     = "${element(data.template_file.interface-conf.*.rendered, count.index)}"

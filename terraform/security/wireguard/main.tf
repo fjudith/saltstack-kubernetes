65d0c6af23a9a56e1349385e19b1521084a8b8d8
Florian JUDITH <florian.judith.b@gmail.com>
2020-06-14 19:12:47 +0200

    Wireguard: added linux headers installation

--- terraform/security/wireguard/main.tf
+++ terraform/security/wireguard/main.tf
@@ -28,10 +28,10 @@ resource "null_resource" "wireguard" {
 
   provisioner "remote-exec" {
     inline = [
-      "apt-get install -yqq libmnl-dev libelf-dev pkg-config software-properties-common build-essential",
+      "apt-get install -yqq libmnl-dev libelf-dev pkg-config software-properties-common build-essential linux-headers-$(uname -r)",
       "add-apt-repository -y ppa:wireguard/wireguard",
       "apt-get update -yqq",
-      "apt-get install -yqq wireguard wireguard-linux-compat",
+      "apt-get install -yqq wireguard",
     ]
   }
 
